e42a86f1e9f20d540c95e80ddacc1f04c20e031c
YULONG94 <37132894+YULONG94@users.noreply.github.com>
2019-03-05 10:24:19 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,18 +3,19 @@ import numpy as np
 # DATASET PARAMETERS
 TRAIN_DIR = 'D:/VOCdata/VOC2012AUG/'
 VAL_DIR = TRAIN_DIR
-TRAIN_LIST = ['D:/VOCdata/VOC2012AUG/train_aug.txt'] * 3
-VAL_LIST = ['D:/VOCdata/VOC2012AUG/val.txt'] * 3
-SHORTER_SIDE = [350] * 3
-CROP_SIZE = [500] * 3
+stages = 3
+TRAIN_LIST = ['D:/VOCdata/VOC2012AUG/train_aug.txt'] * stages
+VAL_LIST = ['D:/VOCdata/VOC2012AUG/val.txt'] * stages
+SHORTER_SIDE = [350] * stages
+CROP_SIZE = [500] * stages
 NORMALISE_PARAMS = [1. / 255,  # SCALE
                     np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3)),  # MEAN
                     np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3))]  # STD
-BATCH_SIZE = [4] * 3
+BATCH_SIZE = [4] * stages
 NUM_WORKERS = 16
-NUM_CLASSES = [21] * 3
-LOW_SCALE = [0.5] * 3
-HIGH_SCALE = [2.0] * 3
+NUM_CLASSES = [21] * stages
+LOW_SCALE = [0.5] * stages
+HIGH_SCALE = [2.0] * stages
 IGNORE_LABEL = 255
 
 # ENCODER PARAMETERS
@@ -23,22 +24,22 @@ ENC_PRETRAINED = False  # pre-trained on ImageNet or randomly initialised
 
 # GENERAL
 EVALUATE = False
-FREEZE_BN = [False] * 3
-NUM_SEGM_EPOCHS = [100] * 3
+FREEZE_BN = [False] * stages
+NUM_SEGM_EPOCHS = [100] * stages
 PRINT_EVERY = 10
 RANDOM_SEED = 42
 SNAPSHOT_DIR = './ckpt/'
 CKPT_PATH = './ckpt/checkpoint.pth.tar'
 MODEL_PATH = './models/my_model.pth'
-VAL_EVERY = [5] * 3  # how often to record validation scores
+VAL_EVERY = [5] * stages  # how often to record validation scores
 
 # OPTIMISERS' PARAMETERS
 LR_ENC = [5e-4, 2.5e-4, 1e-4]  # TO FREEZE, PUT 0
 LR_DEC = [5e-3, 2.5e-3, 1e-3]
-MOM_ENC = [0.9] * 3  # TO FREEZE, PUT 0
-MOM_DEC = [0.9] * 3
-WD_ENC = [1e-5] * 3  # TO FREEZE, PUT 0
-WD_DEC = [1e-5] * 3
+MOM_ENC = [0.9] * stages  # TO FREEZE, PUT 0
+MOM_DEC = [0.9] * stages
+WD_ENC = [1e-5] * stages  # TO FREEZE, PUT 0
+WD_DEC = [1e-5] * stages
 OPTIM_DEC = 'sgd'
 
 palette = [(0, 0, 0),
